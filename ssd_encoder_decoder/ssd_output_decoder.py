0459828e2d4d9461807f8f557cfbd61408d1429f
patrickeala <patrick@eala.asia>
2020-04-06 17:51:15 +0800

    fixed adrian nms names

--- ssd_encoder_decoder/ssd_output_decoder.py
+++ ssd_encoder_decoder/ssd_output_decoder.py
@@ -27,20 +27,20 @@ from bounding_box_utils.bounding_box_utils import iou, convert_coordinates
 
 def adrian_nms(boxes, overlapThresh=0.45):
 	# if there are no boxes, return an empty list
-	if len(boxes) == 0:
-		return []
+    if len(boxes) == 0:
+        return []
 	# if the bounding boxes integers, convert them to floats --
 	# this is important since we'll be doing a bunch of divisions
-	if boxes.dtype.kind == "i":
-		boxes = boxes.astype("float")
+    if boxes.dtype.kind == "i":
+        boxes = boxes.astype("float")
 	# initialize the list of picked indexes	
-	pick = []
+    pick = []
 	# boxes [confidence, xmin, ymin, xmax, ymax]
 	# grab the coordinates of the bounding boxes
-	x1 = boxes[:,1]
-	y1 = boxes[:,3]
-	x2 = boxes[:,2]
-	y2 = boxes[:,4]
+    x1 = boxes[:,1]
+    y1 = boxes[:,3]
+    x2 = boxes[:,2]
+    y2 = boxes[:,4]
 	# compute the area of the bounding boxes and sort the bounding
 	# boxes by the bottom-right y-coordinate of the bounding box
     area = (x2 - x1 + 1) * (y2 - y1 + 1)
@@ -48,36 +48,36 @@ def adrian_nms(boxes, overlapThresh=0.45):
     print("y2: ", y2)
 	# keep looping while some indexes still remain in the indexes
 	# list
-	while len(idxs) > 0:
+    while len(idxs) > 0:
 		# grab the last index in the indexes list and add the
 		# index value to the list of picked indexes
-		print("START")
+        print("START")
         print("idxs: ", idxs)
         last = len(idxs) - 1
-		i = idxs[last]
-		pick.append(i)
+        i = idxs[last]
+        pick.append(i)
         print("pick: ", pick)
 		# find the largest (x, y) coordinates for the start of
 		# the bounding box and the smallest (x, y) coordinates
 		# for the end of the bounding box
-		xx1 = np.maximum(x1[i], x1[idxs[:last]])
-		yy1 = np.maximum(y1[i], y1[idxs[:last]])
-		xx2 = np.minimum(x2[i], x2[idxs[:last]])
-		yy2 = np.minimum(y2[i], y2[idxs[:last]])
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
 		# compute the width and height of the bounding box
-		w = np.maximum(0, xx2 - xx1 + 1)
-		h = np.maximum(0, yy2 - yy1 + 1)
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
 		# compute the ratio of overlap
-		overlap = (w * h) / area[idxs[:last]]
+        overlap = (w * h) / area[idxs[:last]]
         print("overlap: ", overlap)
 		# delete all indexes from the index list that have
-		idxs = np.delete(idxs, np.concatenate(([last],
-			np.where(overlap > overlapThresh)[0])))
+        idxs = np.delete(idxs, np.concatenate(([last],
+            np.where(overlap > overlapThresh)[0])))
         print("END")
 	# return only the bounding boxes that were picked using the
 	# integer data type
 	# return boxes[pick]
-	return boxes[pick].astype("int")
+    return boxes[pick].astype("int")
 
 
 
